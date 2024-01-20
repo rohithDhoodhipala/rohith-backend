750349c389f0bd2d6c8250c2127d7e774c62ecfb
rohithDhoodhipala <rohith.dhoodhipala@octanner.com>
2024-01-20 15:16:11 +0530

    intial spring boot files

---
+++ src/main/java/com/example/rohithbackend/RohithBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rohithbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RohithBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RohithBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rohithbackend/RohithBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rohithbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RohithBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
